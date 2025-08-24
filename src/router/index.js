5fc20bdb1b7b519efa133197016c2cc6af3cfebc
gustavonikov <gustavonikov@gmail.com>
2025-08-24 00:40:06 -0300

    feat: set order details page to home route

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
+import OrderDetailsPage from '@/pages/OrderDetailsPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: OrderDetailsPage
+    }
+  ],
 })
 
 export default router
