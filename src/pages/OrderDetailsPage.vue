b8a115500992db361a1c67015dcf9c23713289d8
gustavonikov <gustavonikov@gmail.com>
2025-08-25 03:12:47 -0300

    feat: import components into the page

--- src/pages/OrderDetailsPage.vue
+++ src/pages/OrderDetailsPage.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="order-details-page">
+    <pre-order-header />
+
+    <supplier-info />
+
+    <addresses-section />
+  </div>
 </template>
 
 <script setup>
+import PreOrderHeader from '@/components/PreOrderHeader.vue'
+import SupplierInfo from '@/components/SupplierInfo.vue'
+import AddressesSection from '@/components/AddressesSection.vue'
 </script>
 
 <style lang="scss" scoped>
+.order-details-page {
+  width: 100%;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+}
 </style>
