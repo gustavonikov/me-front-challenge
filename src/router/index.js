19e179adde0dfff023d4b1d3fe7839be0e9d5fed
gustavonikov <gustavonikov@gmail.com>
2025-08-27 01:07:49 -0300

    fix: router config

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,16 @@ import OrderDetailsPage from '@/pages/OrderDetailsPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
-      component: OrderDetailsPage
+      redirect: '/orders/1',
+    },
+    {
+      path: '/orders/:id',
+      component: OrderDetailsPage,
+      name: 'order-details'
     }
   ],
 })
