91d8fa72905c480a501828e3b41ef146056cdb23
gustavonikov <gustavonikov@gmail.com>
2025-08-27 05:55:34 -0300

    feat: show error component in case of error

--- src/pages/OrderDetailsPage.vue
+++ src/pages/OrderDetailsPage.vue
@@ -1,29 +1,47 @@
 <template>
   <vPageContainer>
     <div class="order-details-page">
-      <vPreOrderHeader :loading="isPending" :data="header" />
+      <template v-if="hasError">
+        <Error @retry="fetchOrder(orderId)">
+          <template #title>
+            <vText type="headline-h4" class="error-title">Failed to load order details.</vText>
+          </template>
+          <template #message>
+            <vText type="body-semibold" class="error-message">
+              There was an issue retrieving the order information. Please try again.
+            </vText>
+          </template>
+        </Error>
+      </template>
 
-      <vSupplierInfo :loading="isPending" :data="supplier" />
+      <template v-else>
+        <vPreOrderHeader :loading="isPending" :data="header" />
 
-      <vAddressesSection :data="addresses" />
+        <vSupplierInfo :loading="isPending" :data="supplier" />
+
+        <vAddressesSection :data="addresses" />
+      </template>
     </div>
   </vPageContainer>
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+
+import { useOrder } from '@/composables/orders/useOrder'
 
 import vPageContainer from '@/components/ui/PageContainer.vue'
 import vPreOrderHeader from '@/components/orders/PreOrderHeader.vue'
 import vSupplierInfo from '@/components/orders/SupplierInfo.vue'
 import vAddressesSection from '@/components/orders/AddressesSection.vue'
-import { useOrder } from '@/composables/orders/useOrder'
-import { onMounted } from 'vue'
+import Error from '@/components/ui/Error.vue'
+import vText from '@/components/ui/Text.vue'
 
 const route = useRoute()
 const orderId = route.params.id
 
-const { header, supplier, addresses, fetchOrder, isPending } = useOrder()
+const { header, supplier, addresses, fetchOrder, isPending, hasError } = useOrder()
 
 onMounted(() => {
   fetchOrder(orderId)
